7bd629cb9b7d475441c70c1be8d5a8d4b4aa1e86
lokimve7 <lokimve7@gmail.com>
2020-04-15 17:34:37 +0900

    123

---
+++ AnimationTest/Assets/TweenTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.localScale = Vector3.zero;
+            iTween.ScaleTo(gameObject, iTween.Hash(
+                "scale", Vector3.one,
+                "easeType", "easeOutBack",
+                "time", 0.5f));
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {   
+            iTween.MoveBy(gameObject, iTween.Hash(
+                "x", Screen.width,
+                "easeType", "easeInBack",
+                "time", 0.5f));
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            iTween.MoveBy(gameObject, iTween.Hash(
+                "x", -Screen.width,
+                "easeType", "easeOutBounce",
+                "time", 1));
+        }
+    }
+}
